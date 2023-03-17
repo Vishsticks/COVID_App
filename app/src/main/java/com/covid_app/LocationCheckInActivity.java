62c251a2d4052ac4381f5eed5b4c6b114527d530
atulraj <contact.atulraj@gmail.com>
2023-03-17 22:40:18 +0530

    Location check in activity added.

---
+++ app/src/main/java/com/covid_app/LocationCheckInActivity.java
@@ -0,0 +1,14 @@
+package com.covid_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationCheckInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_check_in);
+    }
+}
