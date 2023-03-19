da40f3d72684426020b6d86d9d7924f97392b5a1
atulraj <contact.atulraj@gmail.com>
2023-03-19 22:17:46 +0530

    NearBy messaging implementation.

--- app/src/main/java/com/covid_app/MainActivity.java
+++ app/src/main/java/com/covid_app/MainActivity.java
@@ -1,12 +1,25 @@
 package com.covid_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.BleSignal;
+import com.google.android.gms.nearby.messages.Distance;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
+    private MessageListener messageListener;
+    private Message message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,55 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,LocationCheckInActivity.class));
             }
         });
+        messageListener=new MessageListener() {
+            @Override
+            public void onBleSignalChanged(@NonNull Message message, @NonNull BleSignal bleSignal) {
+                super.onBleSignalChanged(message, bleSignal);
+                Log.e(">>>>>","onBleSignalChanged");
+                Toast.makeText(MainActivity.this, "onBleSignalChanged", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDistanceChanged(@NonNull Message message, @NonNull Distance distance) {
+                super.onDistanceChanged(message, distance);
+                Log.e(">>>>>","onDistanceChanged");
+                Toast.makeText(MainActivity.this, "onDistanceChanged", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFound(@NonNull Message message) {
+                super.onFound(message);
+                Log.e(">>>>>","onFound");
+                Toast.makeText(MainActivity.this, "Found::"+message.getContent().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLost(@NonNull Message message) {
+                super.onLost(message);
+                Log.e(">>>>>","onLost");
+                Toast.makeText(MainActivity.this, "onLost callback", Toast.LENGTH_SHORT).show();
+            }
+        };
+        message=new Message("Hello".getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Nearby.getMessagesClient(this).subscribe(messageListener);
+        Nearby.getMessagesClient(this).publish(message);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Nearby.getMessagesClient(this).unsubscribe(messageListener);
+        Nearby.getMessagesClient(this).unpublish(message);
     }
 }
 
+//1. user 1 and user 2
+//2. Both device will publish the data.(user id).
+//3. Both device came into the range.
+//4. both will receive the user id.
+//5. user 1 -> //
