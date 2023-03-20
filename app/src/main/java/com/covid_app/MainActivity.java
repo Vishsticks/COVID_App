af8ac79945b60e1e60d54047c210ce5e4b011619
Vishan <vishangunatilleka13@gmail.com>
2023-03-20 16:55:24 +0000

    Updated Homepage, guidelines

--- app/src/main/java/com/covid_app/MainActivity.java
+++ app/src/main/java/com/covid_app/MainActivity.java
@@ -45,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.txt_guildlines).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CovidGuidlinesActivity.class));
+            }
+        });
+
 
     }
 }
