05e505bf3a1d4f497836136c651616ac782b6ef5
atulraj <contact.atulraj@gmail.com>
2023-03-20 10:59:26 +0530

    FireStore database added. User profile data saved in FireStore.

--- app/src/main/java/com/covid_app/room/Student.java
+++ app/src/main/java/com/covid_app/room/Student.java
@@ -8,24 +8,57 @@ import androidx.room.PrimaryKey;
 public class Student {
     @PrimaryKey @NonNull
     String id;
-    String fullName;
-    String emailAddress;
+    String name;
+    String email;
     String courseName;
 
     public Student() {
 
     }
 
-    public Student(String id, String fullName, String emailAddress, String courseName) {
+    public Student(String id, String name, String emailAddress, String courseName) {
         this.id = id;
-        this.fullName = fullName;
-        this.emailAddress = emailAddress;
+        this.name = name;
+        this.email = emailAddress;
         this.courseName = courseName;
     }
 
     @NonNull
     @Override
     public String toString() {
-        return ">>Student Id ::" + id + ">>Name ::" + fullName + ">>Email Address ::" + emailAddress + ">>Course Name::" + courseName;
+        return ">>Student Id ::" + id + ">>Name ::" + name + ">>Email Address ::" + email + ">>Course Name::" + courseName;
+    }
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
     }
 }
